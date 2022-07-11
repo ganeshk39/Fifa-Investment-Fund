364dbce96d5ef45c4dfc67912375b9efa0b09b68
Ganesh Koirala <ganeshkoirala369@gmail.com>
2022-07-11 16:02:07 +0400

    fixed

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
-
 const app = express();
-
-const PORT = process.env.PORT;
-
 const path = require("path")
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log("Server is listening on Port" + PORT)
